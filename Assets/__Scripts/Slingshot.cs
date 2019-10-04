ac540fafbec4967fbe2ae17f6627755e9c992bbb
Shaymi-san <Shaymi-san@users.noreply.github.com>
2019-10-04 14:31:59 -0400

    Finishing Coding

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 
 public class Slingshot : MonoBehaviour {
+    static private Slingshot S;
+
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -16,9 +18,19 @@ public class Slingshot : MonoBehaviour {
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     void Awake()
     {
-        Transform launchPointTrans = transform.Find("LaunchPoint");
+        S = this;
+        Transform launchPointTrans = transform.FindChild("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
         launchPos = launchPointTrans.position;
@@ -65,6 +77,10 @@ public class Slingshot : MonoBehaviour {
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             projectile = null;
+            FollowCam.POI = projectile;
+            projectile = null;
+            MissionDemolition.ShotFired();
+            ProjectileLine.S.poi = projectile;
         }
 
     }
